162335bdb484fadac285da3d0635fe4d119ecc81
HarshTntra <harsh.rajput@tntra.io>
2022-09-02 19:22:25 +0530

    login_logout_api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    include ActionController::RequestForgeryProtection
+    protect_from_forgery with: :null_session
+    include ActionController::MimeResponds
 end
+  
