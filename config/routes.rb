f4ede4f1cf4b29e2640a575dd6213ecfde381c31
HarshTntra <harsh.rajput@tntra.io>
2022-09-05 16:12:21 +0530

    Forgot password and reset password

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_for :users, controllers: {
         sessions: 'api/v1/users/sessions',
-        passwords: 'api/v1/users/passwords',
-        registrations: 'api/v1/users/registrations'
+        passwords: 'api/v1/users/passwords'
       }
     end
   end
